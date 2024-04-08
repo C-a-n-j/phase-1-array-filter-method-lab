45ebe6196461393859ac8cae170584f249f59d6e
C-a-n-j <eddymacharia450@gmail.com>
2024-04-08 14:39:43 +0300

    complete-lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(collection, namefilter){
+    return collection.filter(
+        (result) => result.toLowerCase() === namefilter.toLowerCase()
+    );
+}
+
+
+function fuzzyMatch(collection, nameString){
+    return collection.filter((result) => 
+    result.toLowerCase().indexOf(nameString.toLowerCase())===0)
+}
+
+function matchName(collection, nameDetails){
+    return collection.filter((result) => result.name===nameDetails)
+}
